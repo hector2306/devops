4a3821623af6c7de573fa626914fc4580add668c
Hector Mares <hmares@intersysconsulting.com>
2019-02-07 15:55:46 -0600

    third

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var https = require(`http`);
 https.createServer(function(req, res){
-    res.writeHead(200,{`Content-Type`: `Text/plain`})
+    res.writeHead(200,{'Content-Type': 'Text/plain'})
     res.end(`Hello Travis CI from my app`)
     
 }).listen(1337, `127.0.0.1`);
