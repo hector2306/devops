f00187d31d8e175cc75c92149d70629151effe1e
Hector Mares <hmares@intersysconsulting.com>
2019-02-07 15:47:42 -0600

    first commit

---
+++ hello.js
@@ -0,0 +1,7 @@
+var https = require(`http`);
+https.createServer(function(req, res){
+    res.writeHead(200,{`Content-Type`: `Text/plain`})
+    res.end(`Hello Travis CI from my app`)
+    
+}).listen(1337, `127.0.0.1`);
+Console.log(`server is running`);
